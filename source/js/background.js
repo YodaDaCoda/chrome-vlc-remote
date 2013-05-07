712f40336986c991f15c54c06707e527f5f77746
William Pickering <pickeringw+github@gmail.com>
2013-05-07 14:42:06 +1000

    Allow sending any link to VLC.
    Also commented out some unused stuff.

--- source/js/background.js
+++ source/js/background.js
@@ -1,3 +1,21 @@
+/*
+Chrome VLC Remote
+	Copyright (C) 2013  William Pickering
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * Returns a handler which will open a new window when activated.
  */
@@ -6,8 +24,10 @@ chrome.contextMenus.create({
 	"type" : "normal",
 	"contexts" : ["link"],
 	"targetUrlPatterns": 	[
-								"http://www.youtube.com/watch?v=*",
-								"http://www.youtube.com/v/*"
+//								"http://www.youtube.com/watch?v=*",
+//								"http://www.youtube.com/v/*",
+//								"*://*/*.mp3"
+								"<all_urls>"
 							],
 	"onclick" : function(info, tab) {
 					var url = info.linkUrl;
@@ -28,10 +48,23 @@ chrome.contextMenus.create({
 				}
 });
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log(request);
+// Listen for any changes to the URL of any tab.
+// Extensions cannot have both browser-action AND page-action.
+// chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+	// ref http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url
+	// var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+	// var match = tab.url.match(regExp);
+	// if (match && match[7].length == 11){
+		// chrome.pageAction.show(tabId);
+	// }
+	// alert("tabbed");
+// });
+
+
+// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	// console.log(request);
 	//return true;	//make the $.ajax sendResponse callback work
-});
+// });
 
 function execCmd(cmd){
 	console.log("execCmd " + cmd);
